09f09cf965fb895e2c3a22c36c34af3b1687e2cc
ugnius <ugnius@lexita.lt>
2025-04-30 13:45:03 +0300

    Feat: Matuok.lt changes

--- src/main/java/at/rtr/rmbt/constant/Constants.java
+++ src/main/java/at/rtr/rmbt/constant/Constants.java
@@ -7,7 +7,7 @@ public interface Constants {
     Double RMBT_GEO_ACCURACY_DETAIL_LIMIT = 10000D;
     Double RMBT_GEO_DISTANCE_DETAIL_LIMIT = 3000D;
     String RMBT_DEFAULT_LANGUAGE = "en";
-    List<String> RMBT_SUPPORTED_LANGUAGES = List.of("de", "en", "hr", "sv", "pl", "es", "is", "fr", "nb", "cs");
+    List<String> RMBT_SUPPORTED_LANGUAGES = List.of("de", "en", "lt", "sv", "pl", "es", "is", "fr", "nb", "cs");
     String WEASYPRINT_PATH = "/usr/bin/weasyprint";
     String PRINCE_PATH = "/usr/bin/prince";
     String PDF_CONVERTER = "weasyprint";
@@ -16,8 +16,10 @@ public interface Constants {
     String OPENTESTS_INTRADAY_CACHE_NAME = "opentestHourly";
     String OPENTESTS_HISTOGRAM_CACHE_NAME = "opentestHistogram";
     String STATISTICS_STALE_CACHE_NAME = "statisticStaleCache";
-    Long CACHE_STALE_HOURS = 1L;
-    Long CACHE_EXPIRE_HOURS = 6L;
+   // Long CACHE_STALE_HOURS = 1L;
+    //Long CACHE_EXPIRE_HOURS = 6L;
+    Long CACHE_STALE_HOURS = 0L;
+    Long CACHE_EXPIRE_HOURS = 0L;
     Long OPENTESTS_STATISTICS_CACHE_EXPIRE_SECONDS = 300L;
     Long OPENTESTS_INTRADAY_CACHE_EXPIRE_SECONDS = 3600L;
     Long OPENTESTS_HISTOGRAM_CACHE_EXPIRE_SECONDS = 3600L;
